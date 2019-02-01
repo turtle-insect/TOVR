1cb2985b57a54d5135cf846e3d64da438e7e4bbb
turtle-insect <turtle.insect@gmail.com>
2019-02-02 02:49:22 +0900

    refactor

--- TOVR/DataContext.cs
+++ TOVR/DataContext.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace TOVR
 {
